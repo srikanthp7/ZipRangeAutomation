2850d2b96d6762888e22ef04fbda63a2eb2369ab
deepa <deepa@10.0.0.200>
2018-06-24 17:51:10 -0500

    Made seperate classess

---
+++ src/main/java/com/sri/ZipRangeAutomation/MainClass.java
@@ -0,0 +1,22 @@
+package com.sri.ZipRangeAutomation;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainClass 
+{
+  
+	public static void main( String[] args )
+    {	
+    	String[] zipRange = {"[94133,94133]","[94200,94609]","[94600,94699]"}; 
+		//String[] zipRange ={"[49679,52015]","[49800,50000]", "[51500,53479]", "[45012,46937]","[54012,59607]","[45500,45590]", "[45999,47900]","[44000,45000]","[43012,45950]"};
+		String [] outputArray;
+        CreateZipRangeArray zipRangeArray =  new CreateZipRangeArray(zipRange);
+        int[] zipArray = zipRangeArray.buildZipRangeArray();
+        BuildOutputZipRange buildOutputRange = new BuildOutputZipRange(zipArray);
+        outputArray = buildOutputRange.buildOutput();
+        for (int i = 0; i < outputArray.length; i++) {
+			System.out.println("FINAL OUTPUT" + outputArray[i]);
+		}
+        
+    }
+}
