506395cc8c8742b7257dceb101741989842f54c4
srikanth podishetty <srikanthp7@gmail.com>
2018-06-25 16:31:59 -0700

    Update CreateZipRangeArray.java

--- src/main/java/com/sri/ZipRangeAutomation/CreateZipRangeArray.java
+++ src/main/java/com/sri/ZipRangeAutomation/CreateZipRangeArray.java
@@ -2,13 +2,28 @@ package com.sri.ZipRangeAutomation;
 
 import java.util.Arrays;
 
+/**
+ * This class parses the String array into Integer array
+ * @author SPodishe
+ *
+ */
 public class CreateZipRangeArray {
 	
 	private String[] zipRange;
+	/**
+	 * @param zipRange
+	 */
 	public CreateZipRangeArray(String[] zipRange)
 	{
 		this.zipRange= zipRange;
 	}
+	
+	/**
+	 * This function takes the input String Array and parses in to integer array.
+	 * {[94133,94133] [94200,94299] [94600,94699]}
+	 * @return a integer array which contains all the zipcodes  
+	 *  {94133,94133,94200,94299,94600,94699}
+	 */
 	public int[] buildZipRangeArray()
 	{
 		int j=0;
